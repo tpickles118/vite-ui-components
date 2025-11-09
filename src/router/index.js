8c224ec3609b233e01fc813939350e5dbea1d776
tpickles118 <46704998+tpickles118@users.noreply.github.com>
2025-11-09 18:24:11 +0100

    fix linting errors

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
+import Home from '@/views/HomeView.vue'
+import About from '@/views/AboutView.vue'
 import UsersAPI from '@/views/UsersApi.vue'
 import BadgeView from '@/views/daisy-wrapper-views/BadgeView.vue'
 
