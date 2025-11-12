c1295755be2e730b048c34f6db0676d07daa5f23
tpickles118 <46704998+tpickles118@users.noreply.github.com>
2025-11-12 16:02:56 +0100

    appropriate icons implemented

--- src/components/base/DaisyAlert.vue
+++ src/components/base/DaisyAlert.vue
@@ -2,14 +2,29 @@
 import { computed } from 'vue'
 import { ALERT_OPTIONS } from '@/constants'
 import { AlertModel } from '@/components/models/alertModel.js'
-import { InformationCircleIcon, ExclamationCircle, ExclamationTriangle, CheckCircle } from '@heroicons/vue/24/solid'
-
+import {
+    InformationCircleIcon,
+    ExclamationCircleIcon,
+    ExclamationTriangleIcon,
+    CheckCircleIcon,
+} from '@heroicons/vue/24/outline'
 
 const props = defineProps({
     alertModel: {
         type: [Object, AlertModel],
         required: true,
     },
+})
+
+const iconMap = {
+    INFO: InformationCircleIcon,
+    SUCCESS: CheckCircleIcon,
+    WARNING: ExclamationTriangleIcon,
+    ERROR: ExclamationCircleIcon,
+}
+
+const currentIcon = computed(() => {
+    return iconMap[props.alertModel?.type] || null
 })
 
 const classes = computed(() => {
@@ -26,23 +41,20 @@ const classes = computed(() => {
     if (am.isOutline) cls.push(ALERT_OPTIONS.OUTLINE)
     if (am.isDash) cls.push(ALERT_OPTIONS.DASH)
     if (am.isSoft) cls.push(ALERT_OPTIONS.SOFT)
-    
+
     // Direction
     if (am.direction) {
         const dirVal = ALERT_OPTIONS.DIRECTION?.[am.direction] ?? am.direction
         if (dirVal) cls.push(dirVal)
     }
-    
+
     return cls
 })
 </script>
 
 <template>
     <div :id="alertModel?.id" :class="classes" role="alert" aria-label="alert">
-        <InformationCircleIcon v-if="info" />
-        <ExclamationCircle v-else-if="error" />
-        <ExclamationTriangle v-else-if="warning" />
-        <CheckCircle v-else-if="success" />
+        <component :is="currentIcon" v-if="currentIcon" class="size-5" />
         <slot>{{ alertModel?.message }}</slot>
     </div>
 </template>
